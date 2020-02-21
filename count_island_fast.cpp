31818be7dd54d7a1f7a960971efad2677bd32be9
ErhanOnur <onursendag@gmail.com>
2020-02-21 20:55:25 +0300

    Create count_island_fast.cpp
     It is a new algorithm by using vector and unordered_set without saving island data in island objects..

---
+++ count_island_fast.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+typedef  int island_id;
+
+struct island_range {
+  int x_beg;
+  int x_end;
+  island_id id;
+  island_range(int xb, int xe, island_id id) : x_beg{xb}, x_end{xe}, id{id}  {}
+};
+
+int main()
+{	
+  vector<vector<int>> vmap {
+                            { 1, 1, 1, 0, 1, 0, 0, 0 },
+                            { 0, 0, 0, 0, 1, 0, 1, 0 },
+                            { 1, 1, 1, 0, 1, 1, 1, 0 }
+                            };
+
+  vector<island_range>* vec_top = new vector<island_range>{};
+  vector<island_range>* vec_current = new vector<island_range>{};
+  vec_top->reserve(vmap[0].size()/2+1);
+  vec_current->reserve(vmap[0].size()/2+1);
+  unordered_set<island_id> unique_islands;
+
+  island_id island_id_count = 0;
+
+  for (auto &vec: vmap)
+  {
+    for (auto &p: vec)
+      cout << p << " ";
+    cout << endl;
+  }
+
+  for (int y = 0 ; y < (int)vmap.size(); y++)
+  {
+    auto& vt = *vec_top;
+    auto& vc = *vec_current;
+
+    auto vtop_itr = vt.begin();
+
+    //cout << "\nanalizing line number " << y+1 << endl;
+
+    for (int x = 0 ; x < (int)vmap[0].size(); x++)
+    {
+      int& value = vmap[y][x];
+      //cout << "*** analyzing the point ( " << x << "," << y << ") with value: " << value <<endl; 
+
+      if (value)
+      {
+        island_id left=0, top=0;
+
+        while (vtop_itr != vt.end())
+        {
+          //cout << "checking top rage with island id :" << vtop_itr->id <<endl; 
+          if (x >= vtop_itr->x_beg && x <= vtop_itr->x_end)
+          {
+            top = vtop_itr->id;
+          }
+          if (x > vtop_itr->x_end)
+          {
+            ++vtop_itr;
+            //cout << "jumping to the next top range... " <<endl; 
+            continue;
+          }
+          break;
+        }
+
+        if (!vc.empty())
+        {
+          if (x-1 == vc.rbegin()->x_end)
+          left = vc.rbegin()->id;
+        }
+
+        //cout << "left adjacent island id: " << left << ", top adjacent island id: " << top << endl; 
+
+        if (left && top) //we have island at top and at left
+        {
+          auto id = vc.rbegin()->id;
+          if (id != vtop_itr->id)
+          {
+            unique_islands.erase(vtop_itr->id);
+            //cout << "two island with different island ids!.. merge happens!..";
+            //cout << " erasing merged island id: " << vtop_itr->id << endl;
+            vtop_itr->id = id;
+          }
+          vc.rbegin()->x_end = x; 
+        } 
+        else if (left) // we have island on left
+        {
+          vc.rbegin()->x_end = x; 
+
+        } else if (top) // we have island on top
+        {
+          vec_current->emplace_back(x,x,top);
+        }
+        else //lonely island
+        {
+          vec_current->emplace_back(x,x, ++island_id_count);
+          unique_islands.insert(island_id_count);
+
+          //cout << "inserting new island id : " << island_id_count << endl;
+        }
+      }
+    }
+
+    //cout <<endl; 
+
+    vec_top->clear();
+    auto tmp = vec_top;
+    vec_top = vec_current;
+    vec_current = tmp;
+
+    /*cout << "vec_top "; 
+    for (auto &p: *vec_top)
+      cout << "(" << p.x_beg << "," << p.x_end << "," << p.id << ")  ";
+    cout << endl;*/
+
+  }
+
+  cout << "Number of island:" << unique_islands.size() << endl;
+
+}
